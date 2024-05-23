44238dcda66cde4dc473087c96bdb7b231831736
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-05-23 11:19:48 +0300

    feature: tailwind, stylse and fonts setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 
+import './styles/index.css'
+import './styles/_boilerplate.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
 import { App } from './App'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
